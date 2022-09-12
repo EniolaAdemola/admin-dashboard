53382d0ab2b9090276d84b91a7aaac91ebf77cb3
Eniola Ademola <it.eniolaademola@gmail.com>
2022-09-12 21:54:32 +0100

    add contextAPI code

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -5,19 +5,41 @@ const StateContext = createContext();
 // initialState of our items should be set to false only if the user
 // clicks thats when it becomes false.
 const initialState = {
-	chat: false,
 	cart: false,
-	userProfile: false,
+	chat: false,
 	notification: false,
+	userProfile: false,
 };
 
 // ContextProvider always have the "children" as property
-// export const ContextProvider = ({children}) = {
-//     return (
-//         // <StateContext.Provider
-//         // // What ever value we pass here
-//         //     value= {{test: 'text'}}
-//         // >
-//         // </StateContext.Provider>
-//     )
-// }
+export const ContextProvider = ({ children }) => {
+	const [activeMenu, setActiveMenu] = useState(true);
+	const [isClicked, setIsClicked] = useState(initialState);
+	const [screenSize, setScreemSize] = useState(undefined);
+
+	const handleClick = (clicked) => {
+		setIsClicked({
+			...initialState,
+			[clicked]: true,
+		});
+	};
+	return (
+		<StateContext.Provider
+			// What ever value we pass here
+			value={{
+				// it's the same as using activeMenu only like i did fo setActiveMenu
+				activeMenu: activeMenu,
+				setActiveMenu,
+				isClicked,
+				setIsClicked,
+				handleClick,
+				screenSize,
+				setScreemSize,
+			}}
+		>
+			{children}
+		</StateContext.Provider>
+	);
+};
+
+export const useStateContext = () => useContext(StateContext);
