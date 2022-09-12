538078dcb866f2195f77b112a8b9165fe86387fa
Eniola Ademola <it.eniolaademola@gmail.com>
2022-09-12 21:52:56 +0100

    updated active menu in sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,9 +5,18 @@ import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { links } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
+// Close side bar when link is clicked on mobile device
 const Sidebar = () => {
-	const activeMenu = true;
+	const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+	const handleCloseSideBar = () => {
+		if (activeMenu && screenSize <= 900) {
+			setActiveMenu(false);
+		}
+	};
+
 	const activeLink =
 		"flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
 	const normalLink =
@@ -20,7 +29,7 @@ const Sidebar = () => {
 					<div className="flex justify-between items-center">
 						<Link
 							to="/"
-							onClick={() => {}}
+							onClick={handleCloseSideBar}
 							className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
 						>
 							<SiShopware />
@@ -29,7 +38,9 @@ const Sidebar = () => {
 						<TooltipComponent content="Menu" position="BottomCenter">
 							<button
 								type="button"
-								onClick={() => {}}
+								onClick={() =>
+									setActiveMenu((prevActiveMenu) => !prevActiveMenu)
+								}
 								className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
 							>
 								<MdOutlineCancel />
@@ -46,7 +57,7 @@ const Sidebar = () => {
 									<NavLink
 										to={`/${link.name}`}
 										key={link.name}
-										onClick={() => {}}
+										onClick={handleCloseSideBar}
 										className={({ isActive }) =>
 											isActive ? activeLink : normalLink
 										}
