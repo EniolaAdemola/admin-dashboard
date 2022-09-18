689d91eebee32cc9836355e336aaa4d101de4df6
Eniola Ademola <it.eniolaademola@gmail.com>
2022-09-18 21:26:06 +0100

    add footer code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Footer = () => {
-	return <div>Footer</div>;
-};
+const Footer = () => (
+	<div className="mt-24 relative">
+		<div>
+			<p className="dark:text-gray-200 text-gray-700 text-center">
+				© 2022 All rights reserved by Eniola Ademola
+			</p>
+		</div>
+	</div>
+);
 
 export default Footer;
